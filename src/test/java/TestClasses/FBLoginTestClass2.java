9d3fce8f7ad56aacbd714c7a8600d0018dfcff15
Dr.Robot <Dr.Robot@DESKTOP-F4S4O5V>
2022-07-09 00:35:33 +0530

    New Class transfer into new_Branch...

---
+++ src/test/java/TestClasses/FBLoginTestClass2.java
@@ -0,0 +1,34 @@
+package TestClasses;
+
+import org.testng.annotations.Test;
+
+import FBPomClasses.FBLoginPom;
+
+public class FBLoginTestClass2 extends FBPojoClass {
+
+	@Test
+	public void runLoginTest() throws InterruptedException
+	{
+		driver.get("https://www.facebook.com/");
+		
+		FBLoginPom obj=new FBLoginPom(driver);
+		
+		obj.clickCreateAccount();
+		Thread.sleep(3000);
+		obj.setFirstName();
+		obj.setLastName();
+		obj.setEmail();
+		obj.reSetEmail();
+		obj.setPass();
+		Thread.sleep(2000);
+		obj.selectDay();
+		obj.selectMonth();
+		obj.selectYear();
+		
+		obj.clickGender();
+		
+		obj.selectPronoun();
+		
+		obj.clicksignUp();
+}
+}
